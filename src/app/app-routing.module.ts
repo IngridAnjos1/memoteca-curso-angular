76308dead529275b0b0af89efd564d7fbd53a1ab
Ingrid Anjos <ingrid_anjos@hotmail.com>
2023-04-30 12:47:31 -0300

    feat: altercao do componente principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   {
@@ -11,16 +12,18 @@ const routes: Routes = [
   },
 
   {
-  path: 'criarPensamento',
-  component: CriarPensamentosComponent
+    path: 'criarPensamento',
+    component: CriarPensamentosComponent
   },
-
   {
     path: 'listarPensamento',
     component: ListarPensamentoComponent
-  },  
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent
+  },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
